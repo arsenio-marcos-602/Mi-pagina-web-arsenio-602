43bc5329b3368d89ab3fd963358272ee2105904a
arsenio-marcos-602 <arseniomarcos25@gmail.com>
2026-03-27 09:57:24 -0600

    actualizacion

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrarmensaje (cuida a tus mascotas) {
+    let mensaje=document.getElementById("mensaje");
+}
+mensaje.innerHTML= "cuida y proteje a tus mascotas";
+mensaje.style.display="block";
+
+setTimeout (c) => {
+    mensaje.style.opacity="1";
+    mensaje.style.transfor="translatey(0)";
+},50);
+
+}
