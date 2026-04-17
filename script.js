b1526347001d34fe9e0cc949a4b6890cd9b086ec
arsenio-marcos-602 <arseniomarcos25@gmail.com>
2026-04-17 09:37:26 -0600

    actualizacion

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ setTimeout(() => {
 }
 
 
+
+function mostrarmensaje2 () {
+    let mensaje = document.getElementById("mensaje2");
+
+mensaje.innerHTML = "chismosooooooooo";
+mensaje.style.display = "block";
+
+setTimeout(() => {
+    mensaje.style.opacity = "1";
+    mensaje.style.transfor = "translateY(0)";
+},50);
+
+}
+
+
